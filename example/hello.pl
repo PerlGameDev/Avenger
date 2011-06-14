111921d9ece272495b3da0e6653c7f423cb571e4
kthakore <thakore.kartik@gmail.com>
2011-06-14 11:18:11 -0400

    Clearing the stuff

--- example/hello.pl
+++ example/hello.pl
@@ -2,12 +2,13 @@ use lib '../lib';
 use Avenger title => 'Hello, World!';
 
 my $rect = rect( 10, 10, 100, 100 );
-
+my $app_rect = rect( 0, 0, app->w, app->h);
 move {
     $rect->x( $rect->x + 1 );
 };
 
 show {
+	app->draw_rect( $app_rect, 0x0 );
     app->draw_rect( $rect, 0x112244FF );
     app->update;
 };
