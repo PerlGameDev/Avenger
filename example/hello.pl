6854ccb380572ad0baa8c21f2fa7a3a57f47fefa
Breno G. de Oliveira <garu@cpan.org>
2011-06-15 02:00:45 -0300

    improving the example a bit

--- example/hello.pl
+++ example/hello.pl
@@ -2,10 +2,34 @@ use lib '../lib';
 use Avenger title => 'Hello, World!';
 
 my $rect = rect( 10, 10, 100, 100 );
+my $state = '';
+my $speed = 10;
+
 my $app_rect = rect( 0, 0, app->w, app->h);
-move {
-    $rect->x( $rect->x + 1 );
+
+event 'key_down' => sub {
+    $state = shift;
 };
+
+event 'key_up' => sub {
+    $state = '' if $state eq shift;
+};
+
+move {
+    if ($state eq 'left') {
+        $rect->x( $rect->x - $speed );
+    }
+    elsif ($state eq 'right') {
+        $rect->x( $rect->x + $speed );
+    }
+    elsif ($state eq 'up') {
+        $rect->y( $rect->y - $speed );
+    }
+    elsif ($state eq 'down') {
+        $rect->y( $rect->y + $speed );
+    }
+};
+
 
 show {
 	app->draw_rect( $app_rect, 0x0 );
