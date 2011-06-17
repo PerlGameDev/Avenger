61ee328fb0d9e02eb280279359b283b91dd3b5ab
Breno G. de Oliveira <garu@cpan.org>
2011-06-17 02:36:58 -0300

    fixating dt at 0.02

--- lib/Avenger.pm
+++ lib/Avenger.pm
@@ -32,7 +32,8 @@ sub import {
         $app = SDLx::App->new(
             eoq => 1,
             flags => SDL_DOUBLEBUF | SDL_HWSURFACE,
-            delay => 100, 
+            delay => 100,
+            dt    => 0.02,
             %properties
         );
         $app->stash->{_avenger}{BASE} = $caller;
