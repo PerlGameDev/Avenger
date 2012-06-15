be32213b88e65b3fdd278033f9063fcffabedc3c
Kartik Thakore <thakore.kartik@gmail.com>
2012-06-15 18:08:13 -0400

    Calling all the collision subs

--- lib/Avenger/Body.pm
+++ lib/Avenger/Body.pm
@@ -6,29 +6,39 @@ use strict;
 use warnings;
 
 sub new {
-    my ($class, $world, %args) = @_;
+    my ( $class, $world, %args ) = @_;
 
-    my $width    = $args{width}    || $args{w} || 20;
-    my $height   = $args{height}   || $args{h} || 20;
-    my $x        = $args{x}        || 0;
-    my $y        = $args{y}        || 0;
-    my $type     = $args{type}     || 'static';
+    my $width  = $args{width}  || $args{w} || 20;
+    my $height = $args{height} || $args{h} || 20;
+    my $x      = $args{x}      || 0;
+    my $y      = $args{y}      || 0;
+    my $type   = $args{type}   || 'static';
     my $density;
     my $friction = $args{friction} || 0.3;
-    my $collided = $args{collided} || sub { }; 
-    my $half_w   = $width  / 2;
-    my $half_h   = $height / 2;
-    
+    my $collided = $args{collided} || {};
+
+    $collided->{begin} = sub { }
+      unless $collided->{being};
+    $collided->{begin} = sub { }
+      unless $collided->{being};
+    $collided->{presolve} = sub { }
+      unless $collided->{presolve};
+    $collided->{postsolve} = sub { }
+      unless $collided->{postsolve};
+
+    my $half_w = $width / 2;
+    my $half_h = $height / 2;
+
     my $body_def = Box2D::b2BodyDef->new;
     $body_def->position->Set( $x, $y );
-    if ($type ne 'static' ) {
-        $body_def->type( Box2D::b2_dynamicBody );
+    if ( $type ne 'static' ) {
+        $body_def->type(Box2D::b2_dynamicBody);
         $density = $args{density} || 1.0;
     }
 
-    my $body = $world->CreateBody( $body_def );
+    my $body = $world->CreateBody($body_def);
 
-    if ($type eq 'bullet') {
+    if ( $type eq 'bullet' ) {
         $body->SetBullet(1);
     }
 
@@ -36,35 +46,34 @@ sub new {
     $box->SetAsBox( $half_w, $half_h );
 
     my $fixture = Box2D::b2FixtureDef->new;
-    $fixture->shape( $box );
+    $fixture->shape($box);
 
     $fixture->density($density) if $density;
     $fixture->friction($friction);
-    my $fix = $body->CreateFixtureDef( $fixture );
-
+    my $fix = $body->CreateFixtureDef($fixture);
 
     my $self = bless {
-        _body   => $body,
-        _width  => $width,
-        _height => $height,
-        _half_w => $half_w,
-        _half_h => $half_h,
-        _app_h  => $args{app_h},
-        _fixture => $fix,
+        _body     => $body,
+        _width    => $width,
+        _height   => $height,
+        _half_w   => $half_w,
+        _half_h   => $half_h,
+        _app_h    => $args{app_h},
+        _fixture  => $fix,
         _collided => $collided
     }, $class;
     $fix->SetUserData( { parent => $self } );
 
-    return $self; 
+    return $self;
 }
 
-sub w { return $_[0]->{_width}  }
+sub w { return $_[0]->{_width} }
 sub h { return $_[0]->{_height} }
 
 sub base { return { $_[0]->{_body}, $_[0]->{_fix} } }
 
 sub awake {
-    my ($self, $awake) = @_;
+    my ( $self, $awake ) = @_;
     if ($awake) {
         $self->{_body}->SetAwake($awake);
         return $awake;
@@ -75,20 +84,20 @@ sub awake {
 }
 
 sub position {
-    my ($self, @pos) = @_;
+    my ( $self, @pos ) = @_;
     my $body = $self->{_body};
 
     my $position = $body->GetPosition;
-    my ($x, $y) = ( $position->x(), $position->y() );
+    my ( $x, $y ) = ( $position->x(), $position->y() );
 
     if (@pos) {
         $x ||= $pos[0];
         $y ||= $pos[1];
         my $angle = $body->GetAngle();
-        my $vec = Box2D::b2Vec2->new($x,$y);
+        my $vec = Box2D::b2Vec2->new( $x, $y );
         $body->SetTransform( $vec, $angle );
     }
-    return ($x, $y);
+    return ( $x, $y );
 }
 
 sub y {
@@ -102,27 +111,27 @@ sub x {
 }
 
 sub velocity {
-    my ($self, @vel) = @_;
+    my ( $self, @vel ) = @_;
     if (@vel) {
         my $vel = Box2D::b2Vec2->new(@vel);
         $self->{_body}->SetLinearVelocity($vel);
     }
     my $velocity = $self->{_body}->GetLinearVelocity();
-    return ($velocity->x, $velocity->y);
+    return ( $velocity->x, $velocity->y );
 }
 
 sub rect {
     my $pos = $_[0]->{_body}->GetPosition;
-    return SDLx::Rect->new( $pos->x - $_[0]->{_half_w},
-                           $_[0]->{_app_h} - $pos->y - $_[0]->{_half_h},
-                           $_[0]->{_width},
-                           $_[0]->{_height}
+    return SDLx::Rect->new(
+        $pos->x - $_[0]->{_half_w},
+        $_[0]->{_app_h} - $pos->y - $_[0]->{_half_h},
+        $_[0]->{_width}, $_[0]->{_height}
     );
 }
 
 sub collided {
     my $self = shift;
-    $self->{_collided}->( @_ ); 
+    $self->{_collided}->(@_);
 
 }
 
--- lib/Avenger/ContactListener.pm
+++ lib/Avenger/ContactListener.pm
@@ -6,31 +6,51 @@ use base qw(Box2D::b2ContactListener);
 sub BeginContact {
     my ( $self, $contact ) = @_;
 
-# Do something
+    my $body_a = $contact->GetFixtureA()->GetUserData()->{parent};
+    my $body_b = $contact->GetFixtureB()->GetUserData()->{parent};
+
+    #callback to each bodies collide
+    $body_a->collided->{begin}->( $body_b, $contact, $c_impulse );
+    $body_b->collided->{begin}->( $body_a, $contact, $c_impulse );
+
+    # Do something
 }
 
 sub EndContact {
     my ( $self, $contact ) = @_;
 
-# Do something
+    my $body_a = $contact->GetFixtureA()->GetUserData()->{parent};
+    my $body_b = $contact->GetFixtureB()->GetUserData()->{parent};
+
+    #callback to each bodies collide
+    $body_a->collided->{end}->( $body_b, $contact, $c_impulse );
+    $body_b->collided->{end}->( $body_a, $contact, $c_impulse );
+
+    # Do something
 }
 
 sub PreSolve {
     my ( $self, $contact, $manifold ) = @_;
+    my $body_a = $contact->GetFixtureA()->GetUserData()->{parent};
+    my $body_b = $contact->GetFixtureB()->GetUserData()->{parent};
 
-# Do something
+    #callback to each bodies collide
+    $body_a->collided->{postsolve}->( $body_b, $contact, $c_impulse );
+    $body_b->collided->{postsolve}->( $body_a, $contact, $c_impulse );
+
+    # Do something
 }
 
 sub PostSolve {
     my ( $self, $contact, $impulse ) = @_;
-                                       my $body_a = $contact->GetFixtureA()->GetUserData()->{parent};       
-                                       my $body_b = $contact->GetFixtureB()->GetUserData()->{parent};       
-                                        #callback to each bodies collide 
-                                        $body_a->collided( $body_b, $contact, $c_impulse);
-                                        $body_b->collided( $body_a, $contact, $c_impulse);
-# Do something
+    my $body_a = $contact->GetFixtureA()->GetUserData()->{parent};
+    my $body_b = $contact->GetFixtureB()->GetUserData()->{parent};
+
+    #callback to each bodies collide
+    $body_a->collided->{postsolve}->( $body_b, $contact, $c_impulse );
+    $body_b->collided->{postsolve}->( $body_a, $contact, $c_impulse );
+
+    # Do something
 }
-
-
 
 1;
